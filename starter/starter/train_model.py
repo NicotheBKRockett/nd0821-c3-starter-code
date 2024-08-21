e0662edf04f09666464013efcc179a1ec2757902
NicotheBKRockett <100208785+NicotheBKRockett@users.noreply.github.com>
2024-08-21 16:04:33 +0200

    Project Status 21/08

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -6,12 +6,19 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics
+from ydata_profiling import ProfileReport
+
 from sklearn.preprocessing import OneHotEncoder
 # Add code to load in the data.
 data = pd.read_csv("cleaned_data_dropna.csv")
+
 #print(data.info())
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+
+# EDA
+
+#profile = ProfileReport(data, title="Profiling Report")
+#print(profile)
 
 cat_features = [
     "workclass",
@@ -23,15 +30,17 @@ cat_features = [
     "sex",
     "native-country",
 ]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+
+
+X, y, encoder, lb = process_data(
+    data, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
-X_test, y_test, _, _ = process_data(
-    test, categorical_features=cat_features, label="salary", training=True
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # Train and save a model.
 Logistic_regression = train_model(X_train,y_train)
-predictions = Logistic_regression.predict(X_train)
+#print(X_test.shape)
+#print(X_train.shape)
+predictions = Logistic_regression.predict(X_test)
 #print(predictions)
