af916b5af27c9787fee47557ff1a45d16c8e3c60
NicotheBKRockett <100208785+NicotheBKRockett@users.noreply.github.com>
2024-08-21 14:34:19 +0200

    Project Status 21/08

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -4,9 +4,11 @@ from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
 import pandas as pd
+from ml.data import process_data
+from sklearn.preprocessing import OneHotEncoder
 # Add code to load in the data.
 data = pd.read_csv("cleaned_data_dropna.csv")
-print(data.info())
+#print(data.info())
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -25,5 +27,7 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
-
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=True
+)
 # Train and save a model.
