219f1ac897961683c2fa70cbf9fb50523e7bed84
NicotheBKRockett <100208785+NicotheBKRockett@users.noreply.github.com>
2024-08-22 08:31:35 +0200

    Project Status 22/08 - Implementation saving or loading SVM model

--- starter/starter/testfunctions_train_model.py
+++ starter/starter/testfunctions_train_model.py
@@ -1,7 +1,14 @@
 import pytest
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import pickle
+import logging
+from starter.ml.data import process_data
+from starter.ml.model import train_model, compute_model_metrics, inference
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='pytests.log', level=logging.INFO)
 
 @pytest.fixture
 def data():
@@ -30,5 +37,20 @@ def test_loaddata():
     filename = "my_model.pickle"
     try:
         model = pickle.load(open(filename, "rb"))
+        logger.info('SUCCES test_loaddata: Loading model')
     except:
-        print('Error occured during loading model')
+        logger.info('FAILED test_loaddata: Error occured during loading model')
+
+def test_metrics(data):
+    X_train, X_test, y_train, y_test = data
+    filename = "my_model.pickle"
+    model = pickle.load(open(filename, "rb"))
+    logger.info('SUCCES test_metrics: Data and model loaded')
+    predictions_X_test = inference(model, X_test)
+    precision, recall, fbeta = compute_model_metrics(y_test, predictions_X_test)
+    assert precision >= 0.93
+    logger.info('SUCCES test_metrics: accuracy good enough')
+    assert recall <= 0.2
+    logger.info('SUCCES test_metrics: recall good enough')
+    assert fbeta <= 0.2
+    logger.info('SUCCES test_metrics: fbeta good enough')
