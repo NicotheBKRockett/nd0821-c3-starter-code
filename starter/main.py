5c56c9559299b326948f39f44aa6f5727c6154b1
NicotheBKRockett <100208785+NicotheBKRockett@users.noreply.github.com>
2024-08-25 21:00:21 +0200

    Project Status 22/08 - Implementation FASTAPI 25/08

--- starter/main.py
+++ starter/main.py
@@ -1,2 +1,36 @@
 # Put the code for your API here.
-print("Test 7")
+#print("Test 7")
+
+from typing import Union
+from pydantic import BaseModel
+
+#def foo(a: Union[list,str], b: int = 5) -> str:
+#    pass
+
+from fastapi import FastAPI
+
+class TaggedItem(BaseModel):
+    name: str
+    tags: Union[str, list]
+    item_id: int
+
+
+# Instantiate the app.
+app = FastAPI()
+
+# Define a GET on the specified endpoint.
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hello dear user! Welcome at the third project of the Udacity ML DevOps Nanodegree Program. I am Nicolas Delay"}
+
+
+@app.post("/data/")
+async def ingest_data(data: Data):
+    if data.feature_1 < 0:
+        raise HTTPException(status_code=400, detail="feature_1 needs to be above 0.")
+    if len(data.feature_2) > 280:
+        raise HTTPException(
+            status_code=400,
+            detail=f"feature_2 needs to be less than 281 characters. It has {len(data.feature_2)}.",
+        )
+    return data
