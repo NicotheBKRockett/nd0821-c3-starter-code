2b5163140a53230d01efba01187a47221e38d10b
NicotheBKRockett <100208785+NicotheBKRockett@users.noreply.github.com>
2024-08-26 19:51:06 +0200

    Project Status 22/08 - Implementation FASTAPI 25/08

--- starter/main.py
+++ starter/main.py
@@ -3,16 +3,50 @@
 
 from typing import Union
 from pydantic import BaseModel
+import pickle
 
 #def foo(a: Union[list,str], b: int = 5) -> str:
 #    pass
 
 from fastapi import FastAPI
+from starter.ml.model import inference
+from starter.ml.data import process_data
 
-class TaggedItem(BaseModel):
-    name: str
-    tags: Union[str, list]
-    item_id: int
+class Item(BaseModel):
+    workclass: str
+    education: str
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    native_country: str
+    age: int
+    fnlwgt: int
+    education_num: int
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 43,
+                "workclass": 'State-gov',
+                "fnlgt": 77516,
+                "education": 'Bachelors',
+                "education_num": 17,
+                "marital_status": "Never-married",
+                "occupation": "Tech-support",
+                "relationship": "Unmarried",
+                "race": "White",
+                "sex": "FMale",
+                "capital_gain": 2000,
+                "capital_loss": 0,
+                "hours_per_week": 35,
+                "native_country": 'United-States'
+            }
+        }
 
 
 # Instantiate the app.
@@ -24,13 +58,11 @@ async def say_hello():
     return {"greeting": "Hello dear user! Welcome at the third project of the Udacity ML DevOps Nanodegree Program. I am Nicolas Delay"}
 
 
-@app.post("/data/")
-async def ingest_data(data: Data):
-    if data.feature_1 < 0:
-        raise HTTPException(status_code=400, detail="feature_1 needs to be above 0.")
-    if len(data.feature_2) > 280:
-        raise HTTPException(
-            status_code=400,
-            detail=f"feature_2 needs to be less than 281 characters. It has {len(data.feature_2)}.",
-        )
-    return data
+@app.post("/data_inference/")
+async def inference_data(data: Item):
+    filename = "my_model.pickle"
+    model = pickle.load(open(filename, "rb"))
+    X, y, encoder, lb = process_data(data, categorical_features=[], label=None, training=True, encoder=None, lb=None)
+    predictions = inference(model, X)
+    return predictions
+
