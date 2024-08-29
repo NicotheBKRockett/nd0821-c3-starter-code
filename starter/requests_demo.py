3450c506f185063ca4de6f6e8689c4e173661d3f
NicotheBKRockett <100208785+NicotheBKRockett@users.noreply.github.com>
2024-08-29 18:32:40 +0200

    update requests.demo: add demonstration of the get method

--- starter/requests_demo.py
+++ starter/requests_demo.py
@@ -22,10 +22,16 @@ data = {
     'native_country': 'United-States'
 }
 
+r1 = requests.get('https://nd0821-c3-starter-code-7d5l.onrender.com/')
+print(r1.status_code)
+print(r1.json())
+assert r1.status_code == 200
+
 data=json.dumps(data)
 r = requests.post(url, data=data)
 
 print(r.status_code)
+print(r.json)
 assert r.status_code == 200
 
 print("Response code: %s" % r.status_code)
