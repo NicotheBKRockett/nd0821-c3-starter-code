3cdc6e5c1694c021e878cd7613b7e5490ddc2165
NicotheBKRockett <100208785+NicotheBKRockett@users.noreply.github.com>
2024-08-29 18:57:26 +0200

    update requests.demo: clean the final code

--- starter/requests_demo.py
+++ starter/requests_demo.py
@@ -24,13 +24,10 @@ r1 = requests.get('https://nd0821-c3-starter-code-0lqu.onrender.com')
 print(r1.status_code)
 print(r1.json())
 assert r1.status_code == 200
-
 data=json.dumps(data)
 r = requests.post(url, data=data)
-
 print(r.status_code)
 print(r.json)
 assert r.status_code == 200
 
-print("Response code: %s" % r.status_code)
-print("Response body: %s" % r.json())
+
